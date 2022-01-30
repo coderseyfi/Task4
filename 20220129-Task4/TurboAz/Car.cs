58451e29a8b41b94b410d2e9d96983e92207cbff
coderseyfi <seyfaddinmn@code.edu.az>
2022-01-30 14:03:01 +0400

    Task-4 TurboAz

---
+++ 20220129-Task4/TurboAz/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurboAz
+{
+    class Car
+    {
+
+        //fields
+        
+        public string brandName;
+        public string cityName;
+        public string model;
+        public int yearofIssue;
+        public string bodyType;
+        public string color;
+        public int engine;
+        public int power;
+        public string fuelType;
+        public double mileage;
+        public string transmitter;
+        public double price;
+
+        public string GetInfoCar()
+        {
+            string output = $"{this.brandName}\n" +
+                $"{this.cityName}\n" +
+                $"{this.model}\n" +
+                $"{this.yearofIssue}\n" +
+                $"{this.bodyType}\n" +
+                $"{this.color}\n" +
+                $"{this.engine}\n" +
+                $"{this.power}\n" +
+                $"{this.fuelType}\n" +
+                $"{this.mileage}\n" +
+                $"{this.transmitter}\n" +
+                $"{this.price}\n";
+
+            return output;
+        }
+
+
+
+       }
+}
+
+
---
+++ 20220129-Task4/TurboAz/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace TurboAz
+{
+    class Program //main programda olmalidir 1 dene
+    {
+       static void Main()
+        {
+            Car car1 = new Car();
+            car1.brandName = "Mercedes";
+            car1.cityName = "Baku";
+            car1.model = "E 300";
+            car1.yearofIssue = 2017;
+            car1.bodyType = "Sedan";
+            car1.color = "black";
+            car1.engine = 2;
+            car1.power = 245;
+            car1.fuelType = "petrol";
+            car1.mileage = 75000;
+            car1.transmitter = "back";
+            car1.price = 55000;
+
+
+            Car car2 = new Car();
+            car2.brandName = "BMW";
+            car2.cityName = "Baku";
+            car2.model = "M3";
+            car2.yearofIssue = 2015;
+            car2.bodyType = "Sedan";
+            car2.color = "white";
+            car2.engine = 3;
+            car2.power = 431;
+            car2.fuelType = "petrol";
+            car2.mileage = 70000;
+            car2.transmitter = "back";
+            car2.price = 35000;
+
+            Car[] cars = new Car[2];
+
+            cars[0] = car1;
+            cars[1] = car2;
+
+            foreach (Car car in cars)
+            {
+                Console.WriteLine(car.GetInfoCar());
+                Console.WriteLine("-----------------------");
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
